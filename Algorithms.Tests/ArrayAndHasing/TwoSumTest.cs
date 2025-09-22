a4d8787ec3a5e9d02354beb6762f12829916e3e3
tuanpnt17 <tuanpntse173039@fpt.edu.vn>
2025-09-22 15:02:30 +0700

    feat(array): add TwoSum algorithm and tests
    Implemented the TwoSum algorithm in ArrayAndHasing namespace. Added unit
    tests for FindTwoSum method to verify correct index pairs are returned for
    given input arrays and targets.

---
+++ Algorithms.Tests/ArrayAndHasing/TwoSumTest.cs
@@ -0,0 +1,15 @@
+using Algorithms.ArrayAndHasing;
+
+namespace Algorithms.Tests.ArrayAndHasing;
+
+public class TwoSumTest
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 7, new int[] { 2, 3 })]
+    [InlineData(new int[] { 3, 4, 5, 6 }, 7, new int[] { 0, 1 })]
+    public void TestFindTwoSum(int[] nums, int target, int[] expected)
+    {
+        var actual = TwoSum.FindTwoSum(nums, target);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Algorithms/ArrayAndHasing/TwoSum.cs
@@ -0,0 +1,23 @@
+namespace Algorithms.ArrayAndHasing;
+
+public class TwoSum
+{
+    public static int[] FindTwoSum(int[] nums, int target)
+    {
+        var differences = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (differences.TryGetValue(nums[i], out int value))
+            {
+                return [value, i];
+            }
+
+
+            var diff = target - nums[i];
+            differences.Add(diff, i);
+        }
+
+        return [];
+    }
+}
